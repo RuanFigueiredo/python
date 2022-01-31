fddc3e5cd7f20f6671ced61d19d89975ce5e6805
ruan <ruanthiago67@gmail.com>
2022-01-31 18:18:10 -0300

    módulos de hipotenusa e truncamentos feitos

---
+++ ex016.py
@@ -0,0 +1,3 @@
+from math import trunc
+num = float(input(' digite aqui um valor numérico:'))
+print('o valor numérico é{} e ele como porção inteira é {}'.format(num, trunc(num)))
---
+++ ex017.py
@@ -0,0 +1,6 @@
+import math
+co = float(input('o cateto oposto é:'))
+ca = float(input('o cateto adjacente é:'))
+h1 = math.hypot(co, ca)
+print('a hipotenusa desse triangulo é:{}'.format(h1))
+
