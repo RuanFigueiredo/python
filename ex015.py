fa8f8846ed93da9dcf469a40a98c5e13c8164538
ruan <ruanthiago67@gmail.com>
2022-01-31 17:07:38 -0300

    exercício 015 adc

--- ex015.py
+++ ex015.py
@@ -2,8 +2,3 @@ dias = int(input('quantos dias andei com o carro?'))
 km = float(input('quantos km eu rodei nele?'))
 preço_aluguel = dias*60 + km*0.15
 print('o preço do aluguel do carro é de {:.2f}R$'.format(preço_aluguel))
-
-
-
-
-
