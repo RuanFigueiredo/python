f5415c340b76d4d7a24975e3c75d93ce1550fe67
Ruan Figueiredo <ruanthiago67@gmail.com>
2022-02-11 07:34:10 -0300

    contagem de letras através da manipulação

---
+++ ex022.py
@@ -0,0 +1,11 @@
+nome = str(input('digite um nome:')).strip()
+print('esse nome em letras maiúsculas fica assim: {}'.format(nome.upper()))
+print('esse nome em letras minúsculas fica assim: {}'.format(nome.lower()))
+print('esse nome tem {} letras'.format(len(nome) - nome.count(' ')))
+print('a primeira palavra do nome tem {} letras'.format(nome.find(' ')))
+
+
+
+
+
+
