7bff9907d0b37dbc5a72c627e5d49b45a556a0df
Ruan Figueiredo <ruanthiago67@gmail.com>
2022-02-17 09:05:39 -0300

    menor número

---
+++ ex033.py
@@ -0,0 +1,9 @@
+a = int(input('digite um número:'))
+b = int(input('digite um número:'))
+c = int(input('digite um número:'))
+menor = a
+if b<a and b <c:
+    menor = b
+if c<b and  c<a:
+    menor =c
+print('\n o menor número é:{}'.format(menor))
