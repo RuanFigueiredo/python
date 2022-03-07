4087ad37a308505f8791554cd79807c2b9b6d662
Ruan Figueiredo <ruanthiago67@gmail.com>
2022-03-07 08:31:22 -0300

    condições aninhadas valores

---
+++ ex038.py
@@ -0,0 +1,8 @@
+numero1 = int(input('digite o primeiro valor:'))
+numero2 = int(input('\ndigite o segundo valor:'))
+if numero1 > numero2:
+    print('\nprimeiro valor é o maior')
+elif numero2 > numero1:
+    print('\nsegundo valor é maior que o primeiro')
+else:
+    print('\n os dois valores sãó iguais')
